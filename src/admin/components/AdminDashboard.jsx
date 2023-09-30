3be96d12a6f9b36dc5bfb61d45fef55392737d82
zwill <zwill_1@hotmail.com>
2023-09-29 23:15:54 -0600

    passing props to social cards

--- src/admin/components/AdminDashboard.jsx
+++ src/admin/components/AdminDashboard.jsx
@@ -1,7 +1,21 @@
 import DashboardHeader from "./DashboardHeader";
 import SocialCard from "./SocialCard";
 
+import useFetch from "../hooks/useFetch";
+
 export default function AdminDashboard() {
+  const {data, loading, error } = useFetch("src/admin/data/SocialData.json");
+  // When getting data, set to "loading".
+  if (loading) return <h1>Loading data...</h1>;
+
+  if (data === null)
+    return <div>No data available. Please try again later.</div>;
+
+  // Return error in console if one shows up
+  if (error) console.log("there is an error " + error);
+
+  console.log("this is the data ", data);
+  console.log(data[0].facebook_name);
   return (
     <>
       <section className="w-full text-center pt-10">
@@ -9,7 +23,7 @@ export default function AdminDashboard() {
       </section>
       <section>
         <div className="w-10/12 mx-auto flex justify-between pt-10 pb-10">
-          <SocialCard bgcolor="bg-gray-600" width="w-full" center="text-center" />
+          <SocialCard dataName={data[0].facebook_name} bgcolor="bg-gray-600" width="w-full" center="text-center" />
           <SocialCard bgcolor="bg-gray-200" width="w-full" center="text-center" />
           <SocialCard bgcolor="bg-gray-400" width="w-full" center="text-center" />
           <SocialCard bgcolor="bg-gray-100" width="w-full" center="text-center" />
--- src/admin/components/SocialCard.jsx
+++ src/admin/components/SocialCard.jsx
@@ -1,11 +1,12 @@
 /* eslint react/prop-types: 0 */
 
-export default function SocialCard({bgcolor, width, center}) {
+export default function SocialCard({bgcolor, width, center, dataName}) {
     const headerStyle = bgcolor + " " + width + " " + center + " " + "pt-8 pb-8 mx-4";
+    console.log("in the card " + dataName);
   return (
     <div className={headerStyle}>
       <div>
-        <span>Social icon / Social handle</span>
+        <span>Social icon / {dataName}</span>
       </div>
       <div className="mt-1">
         <p className="text-6xl font-bold">10205</p>
