ded26082d48ab7c3042c9fb19882bda304593c75
zwill <zwill_1@hotmail.com>
2023-10-14 22:24:58 -0600

    adding avatar images to profiles

--- src/admin/components/SocialCard.jsx
+++ src/admin/components/SocialCard.jsx
@@ -6,6 +6,8 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
+import { useAuth } from "../../contexts/AuthContext";
+
 export default function SocialCard({
   bgcolor,
   border,
@@ -32,19 +34,38 @@ export default function SocialCard({
     "pt-8 pb-8 mx-4 border-2 border-black";
 
   const totalAdjustedFollowers = dataFollowers - dataPrevFollowers;
+
+  const { user } = useAuth();
+
+  //console.log("user " + user.avatar);
+
+
   return (
     <div className={headerStyle}>
       <div>
         <span>
           {dataId == 1653345695 && (
-            <div><InstagramIcon className="instagram-icon-color" /> / <span className="instagram-icon-color">{dataName}</span></div>
+            <>
+              <div className='flex justify-center'><img src={user.avatar} className='rounded-full' /></div>
+              <div><InstagramIcon className="instagram-icon-color" /> / <span className="instagram-icon-color">{dataName}</span></div>
+            </>
           )}
-          {dataId == 16545433 && 
-            <div><YouTubeIcon className="youtube-icon-color" /> / <span className="youtube-icon-color">{dataName}</span></div>}
+          {dataId == 16545433 && (
+            <>
+              <div className='flex justify-center'><img src={user.avatar} className='rounded-full' /></div>
+              <div><YouTubeIcon className="youtube-icon-color" /> / <span className="youtube-icon-color">{dataName}</span></div>
+            </>)}
           {dataId == 165223255295 && (
-            <div><TwitterIcon className="twitter-icon-color" /> / <span className="twitter-icon-color">{dataName}</span></div>
+            <>
+              <div className='flex justify-center'><img src={user.avatar} className='rounded-full' /></div>
+              <div><TwitterIcon className="twitter-icon-color" /> / <span className="twitter-icon-color">{dataName}</span></div>
+            </>
           )}
-          {dataId == 16571806895 && <div><FacebookIcon className="fb-icon-color" /> / <span className="fb-icon-color">{dataName}</span></div>}
+          {dataId == 16571806895 && (
+            <>
+              <div className='flex justify-center'><img src={user.avatar} className='rounded-full' /></div>
+              <div><FacebookIcon className="fb-icon-color" /> / <span className="fb-icon-color">{dataName}</span></div>
+            </>)}
         </span>
       </div>
       <div className="mt-1">
